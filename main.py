f83c67dbbe053dd86d22f85f700e8dee84e933fc
Gerhard Klemola <gerhardcarlklemola@protonmail.com>
2021-12-14 20:51:00 +0200

    lisätty exception handler delete noteen

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def main():
         print("(1) Read the notebook\n(2) Add note\n\
 (3) Edit a note\n(4) Delete a note\n(5) Save and quit")
         userInput = input("Please select one: ")
+
         if userInput == "1":
             notebook = open(filename, "rb")
             while True:
@@ -22,16 +23,19 @@ def main():
                 except EOFError:
                     notebook.close()
                     break
+
         elif userInput == "2":
             notebook = open(filename, "ab")
             noteText = input("Write a new note: ")
             noteText = noteText + ":::" + strftime("%X %x")
             pickle.dump(noteText, notebook)
             notebook.close()
+
         elif userInput == "3":
             count, notes = countnotes.countNotes()
             notebook = open(filename, "wb")
             place = int(input("Which of them will be changed?: "))
+            place -= 1
             print(notes[place])
             noteEdit = input("Give the new note: ")
             noteEdit = noteEdit + ":::" + strftime("%X %x")
@@ -41,26 +45,27 @@ def main():
                 pickle.dump(notes[i], notebook)
                 i += 1
             notebook.close()
+
         elif userInput == "4":
-            print("4")
+            count, notes = countnotes.countNotes()
+            place = int(input("Which of them will be deleted?: "))
+            place -= 1
+            try:
+                print("Deleted note", notes[place])
+                notes.pop(place)
+            except IndexError:
+                print("Invalid index!")
+                continue
+            i = int(0)
+            notebook = open(filename, "wb")
+            while i < len(notes):
+                pickle.dump(notes[i], notebook)
+                i += 1
+            notebook.close()
         elif userInput == "5":
             print("Notebook shutting down, thank you.")
             notebook.close()
             break
-"""
-def countNotes():
-    filename = "notebook.dat"
-    notebook = open(filename, "rb")
-    count = int(0)
-    notes = []
-    while True:
-        try:
-            notes.append(pickle.load(notebook))
-            count += 1
-        except EOFError:
-            print("The list has", count, "notes.", sep=" ")
-            break
-    return count, notes
-"""
+
 if __name__ == "__main__":
     main()
